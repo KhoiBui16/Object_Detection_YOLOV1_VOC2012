10830284acd3bfcfa446ca4aee957b3974608236
KhoiBui16 <khoib1601@gmail.com>
2024-11-22 15:20:41 +0700

    update train.py

--- train.py
+++ train.py
@@ -1,8 +1,58 @@
 import torch
-import torchvision.transforms as transfoms
+import torchvision.transforms as transforms
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataset import PascalVOC2012Dataset
 from loss import YOLOv1Loss
+from utils import (
+    non_max_suppression,
+    mean_average_precision,
+    intersection_over_union,
+    cellboxes_to_boxes,
+    get_bboxes,
+    plot_image,
+    save_checkpoint,
+    load_checkpoint,
+)
 
-# from utils import ()
+
+seed = 123
+torch.manual_seed(seed)
+
+# Hyperparameters etc. 
+LEARNING_RATE = 2e-5
+DEVICE = "cuda" if torch.cuda.is_available else "cpu"
+BATCH_SIZE = 16 # 64 in original paper 
+WEIGHT_DECAY = 0
+EPOCHS = 1000
+NUM_WORKERS = 2
+PIN_MEMORY = True
+LOAD_MODEL = False
+# LOAD_MODEL_FILE = "overfit.pth.tar"
+IMG_DIR = "\Data\VOC2012\JPEGImages"
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, bboxes):
+        for t in self.transforms:
+            img, bboxes = t(img), bboxes
+
+        return img, bboxes
+    
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+
+def train_fn(train_loader, model, optimizer, loss_fn):
+    mean_loss = []
+    
+    for batch_idx, (x, y) in enumerate(train_loader):
+        x, y = x.to(DEVICE), y.to(DEVICE)
+        output = model(x)
+        loss = loss_fn(output, y)
+        mean_loss.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    print(f"Mean loss was {sum(mean_loss) / len(mean_loss)}")
